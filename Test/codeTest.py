3f829db63bfac7f875fd6a6ee6cd53c22e25259f
lchz <lauraz940829@hotmail.com>
2023-11-28 14:39:48 +0200

    First commit

---
+++ Test/codeTest.py
@@ -0,0 +1,16 @@
+import unittest
+from Code.code import *
+
+class TestCalculation(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(add(5,4), 9)
+
+    def test_multiply(self):
+        self.assertEqual(multiply(3,4), 12)
+
+    def test_power(self):
+        self.assertEqual(power(2,8), 256)
+
+if __name__ == '__main__':
+    unittest.main()
